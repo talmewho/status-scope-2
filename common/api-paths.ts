15cdcd487e1da202064f71db0fe741a0663c25bf
talmewho <talmewho@gmail.com>
2025-08-11 13:43:32 +0100

    Added a few shared constants between the client and server and a mock.

---
+++ common/api-paths.ts
@@ -0,0 +1,6 @@
+const apiPaths = {
+  status: '/api/status',
+  mockStatus: '/api/mock-status',
+} as const;
+
+export default apiPaths;
---
+++ common/regions.ts
@@ -0,0 +1,8 @@
+export enum ERegion {
+  UsEast = 'us-east',
+  EuWest = 'eu-west',
+  EuCentral = 'eu-central',
+  UsWest = 'us-west',
+  SaEast = 'sa-east',
+  ApSoutheast = 'ap-southeast',
+}
