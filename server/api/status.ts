1ab50990f6abeaea04a31c62ff35ad2bb1519f3d
talmewho <talmewho@gmail.com>
2025-08-12 00:28:10 +0100

    Added a basic pong sender, utilised when getting a ping from the client.

--- server/api/status.ts
+++ server/api/status.ts
@@ -2,16 +2,16 @@ import type expressWs from 'express-ws';
 import configuration from 'server/services/configuration';
 import { StatusFetcher } from 'server/services/status-fetcher';
 
-const handleStatusSocket: expressWs.WebsocketRequestHandler = async (webSocket, request) => {
+const handleStatusSocket: expressWs.WebsocketRequestHandler = async (webSocket) => {
   const fetcher = new StatusFetcher(
     configuration.statusUrlTemplate,
     configuration.fetchInterval,
   );
 
-  console.log('WebSocket connection established:', request.url);
-
   webSocket.on('message', (data) => {
-    console.log(data);
+    if (data.toString('utf-8') === 'ping') {
+      webSocket.send('pong');
+    }
   });
 
   webSocket.on('close', () => {
