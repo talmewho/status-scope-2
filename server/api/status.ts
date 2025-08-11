3b1d6f4c34ac0c2fe877db05457a467717d9fbfe
talmewho <talmewho@gmail.com>
2025-08-11 13:57:13 +0100

    Used the status fetcher to fetch the data when the websocket is hit.

--- server/api/status.ts
+++ server/api/status.ts
@@ -1,11 +1,23 @@
 import type expressWs from 'express-ws';
+import { StatusFetcher } from 'server/services/status-fetcher';
 
 const handleStatusSocket: expressWs.WebsocketRequestHandler = async (webSocket, request) => {
+  const fetcher = new StatusFetcher(process.env.STATUS_URL_TEMPLATE!);
   console.log('WebSocket connection established:', request.url);
+
   webSocket.on('message', (data) => {
     console.log(data);
   });
-  webSocket.send('"Hello"');
+
+  webSocket.on('close', () => {
+    fetcher.terminate();
+  });
+
+  fetcher.on('data', (data) => {
+    webSocket.send(JSON.stringify(data));
+  });
+
+  webSocket.send(JSON.stringify(await fetcher.fetchAllStatuses()));
 };
 
 export default handleStatusSocket;
