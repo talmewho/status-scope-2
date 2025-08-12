b3a56356b7ff4cfc2cdd3289c1a46851b84df8cf
talmewho <talmewho@gmail.com>
2025-08-12 17:04:10 +0100

    Renamed the file to match its class name.

--- app/network/index.ts
+++ app/network/index.ts
@@ -1,4 +1,4 @@
-import { SocketManager, type TSocketManagerOptions } from './socket';
+import { SocketManager, type TSocketManagerOptions } from './SocketManager';
 
 export function connectSocket<T>(url: string, options: TSocketManagerOptions<T>): () => void {
   const socketManager = new SocketManager(url, options);
